0435da6efb112fde18a2736117e065ce8812119b
JP Kelly <jp_kelly@mac.com>
2026-03-29 20:15:03 -0700

    Phase 2: add cross-build for sdl-clock/alsa-ltc, fix .mk duplicate, update defconfig for Buildroot 2025.02 + Pi 5

--- buildroot-external/package/clock8002/clock8002.mk
+++ buildroot-external/package/clock8002/clock8002.mk
@@ -7,24 +7,38 @@ CLOCK8002_SITE = $(call qstrip,$(BR2_PACKAGE_CLOCK8002_SOURCE_DIR))
 CLOCK8002_SITE_METHOD = local
 CLOCK8002_LICENSE = GPL-3.0
 CLOCK8002_LICENSE_FILES = LICENSE
+CLOCK8002_DEPENDENCIES = host-go sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_ttf libltc
 
-# Prototype install-only package. Build integration for sdl-clock/alsa-ltc
-# should be added once toolchain/runtime dependency decisions are finalized.
+define CLOCK8002_BUILD_CMDS
+	cd $(@D) && \
+		GO111MODULE=on \
+		CGO_ENABLED=1 \
+		GOOS=linux \
+		GOARCH=arm64 \
+		CC="$(TARGET_CC)" \
+		CXX="$(TARGET_CXX)" \
+		$(HOST_DIR)/bin/go build -ldflags "$(TARGET_LDFLAGS)" -o sdl-clock ./cmd/sdl-clock
+	$(TARGET_CC) -O2 -o $(@D)/alsa-ltc $(@D)/alsa-ltc.c -lasound -lltc
+endef
+
 define CLOCK8002_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/opt/clock8002
-	if [ -f $(@D)/sdl-clock ]; then \
-		$(INSTALL) -m 0755 $(@D)/sdl-clock $(TARGET_DIR)/opt/clock8002/sdl-clock; \
-	fi
-	if [ -f $(@D)/alsa-ltc ]; then \
-		$(INSTALL) -m 0755 $(@D)/alsa-ltc $(TARGET_DIR)/opt/clock8002/alsa-ltc; \
-	fi
-	if [ -f $(@D)/clock8002.service ]; then \
-		$(INSTALL) -D -m 0644 $(@D)/clock8002.service \
-			$(TARGET_DIR)/usr/lib/systemd/system/clock8002.service; \
-	fi
-	if [ -f $(@D)/alsa-ltc.service ]; then \
-		$(INSTALL) -D -m 0644 $(@D)/alsa-ltc.service \
-			$(TARGET_DIR)/usr/lib/systemd/system/alsa-ltc.service; \
+	$(INSTALL) -m 0755 $(@D)/sdl-clock $(TARGET_DIR)/opt/clock8002/sdl-clock
+	$(INSTALL) -m 0755 $(@D)/alsa-ltc $(TARGET_DIR)/opt/clock8002/alsa-ltc
+	$(INSTALL) -D -m 0644 $(@D)/clock8002.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/clock8002.service
+	$(INSTALL) -D -m 0644 $(@D)/alsa-ltc.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/alsa-ltc.service
+	$(INSTALL) -D -m 0644 $(@D)/clock.ini.default \
+		$(TARGET_DIR)/boot/piclock/clock.ini
+	$(INSTALL) -D -m 0644 $(@D)/network.ini.default \
+		$(TARGET_DIR)/boot/piclock/network.ini
+	cp -a $(@D)/fonts $(TARGET_DIR)/opt/clock8002/
+	cp -a $(@D)/voices $(TARGET_DIR)/opt/clock8002/
+	cp -a $(@D)/ttf_fonts $(TARGET_DIR)/opt/clock8002/
+	if [ -f $(@D)/oled/oled_daemon.py ]; then \
+		$(INSTALL) -D -m 0755 $(@D)/oled/oled_daemon.py \
+			$(TARGET_DIR)/opt/clock8002/oled/oled_daemon.py; \
 	fi
 endef
 
