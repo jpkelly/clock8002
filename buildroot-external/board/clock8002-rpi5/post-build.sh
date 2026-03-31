183439f43027985a8ec0b526844ac85fc42d0c02
JP Kelly <jp_kelly@mac.com>
2026-03-31 08:35:11 -0700

    buildroot: fix getty symlink - mkdir target dir first

--- buildroot-external/board/clock8002-rpi5/post-build.sh
+++ buildroot-external/board/clock8002-rpi5/post-build.sh
@@ -11,5 +11,6 @@ mkdir -p "${TARGET_DIR}/etc/systemd/system"
 ln -sf /dev/null "${TARGET_DIR}/etc/systemd/system/ModemManager.service"
 
 # Enable login prompt on HDMI (tty1).
+mkdir -p "${TARGET_DIR}/etc/systemd/system/getty.target.wants"
 ln -sf /usr/lib/systemd/system/getty@.service \
 	"${TARGET_DIR}/etc/systemd/system/getty.target.wants/getty@tty1.service"
