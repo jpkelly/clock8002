dbbab85b6a9e39e74d7931d1fd1e2a38ad6baca2
JP Kelly <jp_kelly@mac.com>
2026-04-13 21:48:42 -0700

    buildroot: add SDL3/SDL3_ttf/SDL3_image package definitions
    - sdl3 v3.4.0: KMSDRM backend, ALSA audio, no X11/Wayland/OpenGL/Pulse
    - sdl3-ttf v3.2.2: with harfbuzz + freetype
    - sdl3-image v3.2.6: PNG + JPEG only, no WEBP/AVIF/JXL
    
    All three loaded at runtime via purego dlopen (no CGO at build time).
    Hashes computed from official GitHub release tarballs.
    
    Config.in registration deferred to Phase 3 (full buildroot-external port).

---
+++ buildroot-external/package/sdl3/sdl3.mk
@@ -0,0 +1,36 @@
+################################################################################
+# sdl3
+################################################################################
+
+SDL3_VERSION = 3.4.0
+SDL3_SITE = https://github.com/libsdl-org/SDL/releases/download/release-$(SDL3_VERSION)
+SDL3_SOURCE = SDL3-$(SDL3_VERSION).tar.gz
+SDL3_LICENSE = Zlib
+SDL3_LICENSE_FILES = LICENSE.txt
+SDL3_INSTALL_STAGING = YES
+
+SDL3_DEPENDENCIES = libdrm eudev
+
+SDL3_CONF_OPTS = \
+	-DSDL_SHARED=ON \
+	-DSDL_STATIC=OFF \
+	-DSDL_TESTS=OFF \
+	-DSDL_EXAMPLES=OFF \
+	-DSDL_INSTALL_TESTS=OFF \
+	-DSDL_KMSDRM=ON \
+	-DSDL_OFFSCREEN=ON \
+	-DSDL_X11=OFF \
+	-DSDL_WAYLAND=OFF \
+	-DSDL_COCOA=OFF \
+	-DSDL_DIRECTX=OFF \
+	-DSDL_OPENGL=OFF \
+	-DSDL_OPENGLES=OFF \
+	-DSDL_PULSEAUDIO=OFF \
+	-DSDL_JACK=OFF \
+	-DSDL_OSS=OFF \
+	-DSDL_PIPEWIRE=OFF \
+	-DSDL_RPATH=OFF \
+	-DSDL_ALSA=ON \
+	-DSDL_ALSA_SHARED=OFF
+
+$(eval $(cmake-package))
