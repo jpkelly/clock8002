4f5accedd73079c590e901c8d73363e32c20909e
JP Kelly <jp_kelly@mac.com>
2026-04-21 09:30:50 -0700

    text4: H=240 stride=280 exact 40px gap, fits 1080px

--- v4/cmd/sdl3-clock/text.go
+++ v4/cmd/sdl3-clock/text.go
@@ -293,17 +293,17 @@ func draw4TextClocks(state *clock.State) {
 		if state.Clocks[i].Hidden {
 			continue
 		}
-		y = float32(295 * i)
+		y = float32(280 * i)
 		x = 530
-		numberBox := sdl.FRect{X: x, Y: y, W: 1380, H: 255}
-		textR := sdl.FRect{X: x + 300, Y: y, W: 1380 - 300, H: 255}
+		numberBox := sdl.FRect{X: x, Y: y, W: 1380, H: 240}
+		textR := sdl.FRect{X: x + 300, Y: y, W: 1380 - 300, H: 240}
 		if options.IconsDisable {
 			textR = numberBox
 		}
-		iconR := sdl.FRect{X: x, Y: y, W: 300, H: 255}
+		iconR := sdl.FRect{X: x, Y: y, W: 300, H: 240}
 		x = 10
-		labelR := sdl.FRect{X: x, Y: y, W: 500, H: 85}
-		signalR := sdl.FRect{X: iconR.X - 175, Y: y + 68, W: 120, H: 120}
+		labelR := sdl.FRect{X: x, Y: y, W: 500, H: 80}
+		signalR := sdl.FRect{X: iconR.X - 175, Y: y + 60, W: 120, H: 120}
 		if options.DrawBoxes {
 			rectColor(&numberBox, colors.rowBG[i])
 			rectColor(&labelR, colors.labelBG)
